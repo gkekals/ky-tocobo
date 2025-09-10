7b78febeca31047e22dad6584c926bded93518e4
gkekals <gkekals12@kbu.ac.kr>
2025-09-10 21:34:12 +0900

    1

--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -1,28 +1,32 @@
-import React from "react";
-import instar from "../util/instar";
-import "../styles/components/instargram.scss";
-
+import React from 'react'
+import instar from "../util/instar"
+import "../styles/components/instargram.scss"
 const Instargram = () => {
   return (
-    <section className="instar">
-      <div className="inner instar-inner">
-        <div className="t-wrap">
-          <h2 className="con-tit">Instagram</h2>
-          <p className="txt-4">@tocobo_official</p>
-        </div>
-
-        <ul className="instar-lst" role="list">
-          {instar.map(({ id, image, alt, link }) => (
-            <li key={id} className="instar-item">
-              <a href={link} aria-label={alt}>
-                <img src={image} alt={alt} loading="lazy" />
-              </a>
-            </li>
-          ))}
-        </ul>
+    <div className='inner instar-inner'>
+      <div className="t-wrap">
+        <h2 className="tit">
+          Instargram
+        </h2>
+        <p className="txt-4">
+          @tocobo_official
+        </p>
       </div>
-    </section>
-  );
-};
+      <ul className="instar-lst">
+        {instar.map((i) => (
 
-export default Instargram;
+          <li key={i.id}>
+            <a
+              style={{ backgroundImage: `url(${i.image})` }}
+              alt={i.alt}
+              href={i.link}>
+              {i.alt}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Instargram
